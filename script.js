3d3b4b8209a01a550e54c598cb25c94aced82472
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-04-10 21:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-//complete this code
 class Animal {
-	constructor(species){
-		species: string
-	}
-	makeSound(){
-		conssole.log(`The ${species} makes a sound`);
-	}
+  constructor(species) {
+    this._species = species;
+  }
+  
+  get species() {
+    return this._species;
+  }
+  
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
-	
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
